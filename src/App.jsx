e3ebe89dbfa86ce4a8df271eaa19bf1e73ba3327
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-01-13 18:59:54 +0530

    My local changes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black text-white h-screen w-screen'>
+      <h1>Hello Guys!</h1>
+    </div>
+  )
+}
+
+export default App
