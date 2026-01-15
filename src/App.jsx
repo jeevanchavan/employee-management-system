4d22107965c8d53bd2448d9306a6785df26fd311
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-01-15 18:53:29 +0530

    setting up user data and admin data at the time of log in to check whether it recognise the user data in the storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { getLocalStorage, setLocalStorage } from './utils/localStorage'
+import { AuthContext } from './context/AuthProvider'
+// import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 const App = () => {
 
-  useEffect(() => {
-    // setLocalStorage()
-    getLocalStorage();
-  },)
-  
+  const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext)
+
+  const handleLogin =(email,password)=>{
+    if(email == "admin@me.com" && password == '123'){
+      setUser('admin')
+    }
+    else if(authData && authData.employees.find((e)=> e.email == email && e.password == password)){
+      setUser("employee")
+    }
+    else{
+      alert("Invalid Credentials");
+    }
+  }
 
   return (
     <>
-      <Login />
-      {/* <EmployeeDashboard /> */}
-      {/* <AdminDashboard /> */}
+      {!user ? <Login handleLogin={handleLogin}/>:''}
+      {user == 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   )
 }
