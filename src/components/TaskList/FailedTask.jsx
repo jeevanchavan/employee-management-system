d364c857f8dc3562c09eaf5ffe04318067c312f9
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-01-17 13:38:59 +0530

    within employee dashboard in tasklist is divided into diff components like complete task, new task and stored in tasklist folder, next I have to pass data through files

---
+++ src/components/TaskList/FailedTask.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const FailedTask = () => {
+  return (
+        <div className='flex flex-col shrink-0 items-start rounded-xl h-full w-[360px] bg-red-400 p-5'>
+            <div className='flex justify-between w-full '>
+                <h3 className='bg-red-600 text-sm rounded py-1 px-2'>High</h3>
+                <h4 className='text-sm '>15 11 2025</h4>
+            </div>
+            <h2 className='font-bold text-2xl mt-10'>Complete the Project</h2>
+            <p className='text-sm mt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic eveniet, veritatis beatae quibusdam maxime autem necessitatibus consequatur corrupti culpa laborum!</p>
+            <div className='mt-8 flex w-full'>
+                <button  className='bg-red-800 cursor-pointer border-none outline-none mr-5 text-sm rounded px-2 py-1 w-full'>Failed</button>
+            </div>
+        </div> 
+  )
+}
+
+export default FailedTask
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,40 +1,19 @@
 import React from 'react'
+import AcceptTask from './AcceptTask'
+import NewTask from './NewTask'
+import FailedTask from './FailedTask'
+import CompleteTask from './CompleteTask'
 
-const TaskList = () => {
+const TaskList = ({data}) => {
   return (
     <div id='task' className=' overflow-x-auto w-full h-[60%] flex items-center justify-start gap-5 mt-10 py-5'>
-        <div className='flex flex-col shrink-0 items-start rounded-xl h-full w-[360px] bg-blue-400 p-5'>
-            <div className='flex justify-between w-full '>
-                <h3 className='bg-red-600 text-sm rounded py-1 px-2'>High</h3>
-                <h4 className='text-sm '>15 11 2025</h4>
-            </div>
-            <h2 className='font-bold text-2xl mt-10'>Complete the Project</h2>
-            <p className='text-sm mt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic eveniet, veritatis beatae quibusdam maxime autem necessitatibus consequatur corrupti culpa laborum!</p>
-        </div>
-        <div className='flex flex-col shrink-0 items-start rounded-xl h-full w-[360px] bg-green-400 p-5'>
-            <div className='flex justify-between w-full '>
-                <h3 className='bg-red-600 text-sm rounded py-1 px-2'>High</h3>
-                <h4 className='text-sm '>15 11 2025</h4>
-            </div>
-            <h2 className='font-bold text-2xl mt-10'>Complete the Project</h2>
-            <p className='text-sm mt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic eveniet, veritatis beatae quibusdam maxime autem necessitatibus consequatur corrupti culpa laborum!</p>
-        </div>
-        <div className='flex flex-col shrink-0 items-start rounded-xl h-full w-[360px] bg-yellow-400 p-5'>
-            <div className='flex justify-between w-full '>
-                <h3 className='bg-red-600 text-sm rounded py-1 px-2'>High</h3>
-                <h4 className='text-sm '>15 11 2025</h4>
-            </div>
-            <h2 className='font-bold text-2xl mt-10'>Complete the Project</h2>
-            <p className='text-sm mt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic eveniet, veritatis beatae quibusdam maxime autem necessitatibus consequatur corrupti culpa laborum!</p>
-        </div>
-        <div className='flex flex-col shrink-0 items-start rounded-xl h-full w-[360px] bg-red-400 p-5'>
-            <div className='flex justify-between w-full '>
-                <h3 className='bg-red-600 text-sm rounded py-1 px-2'>High</h3>
-                <h4 className='text-sm '>15 11 2025</h4>
-            </div>
-            <h2 className='font-bold text-2xl mt-10'>Complete the Project</h2>
-            <p className='text-sm mt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic eveniet, veritatis beatae quibusdam maxime autem necessitatibus consequatur corrupti culpa laborum!</p>
-        </div>
+        
+        <AcceptTask />
+        <NewTask/>
+
+        <CompleteTask />
+        <FailedTask/>
+        
         
     </div>
   )
