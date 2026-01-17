d63c7723391363c7a4028c3ea8872c46a970a0eb
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-01-17 13:06:59 +0530

    setting up who is logged in localStorage and passed data through employee dashboad's navbar and tasklistnumbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
@@ -8,14 +8,32 @@ import { AuthContext } from './context/AuthProvider'
 const App = () => {
 
   const [user, setUser] = useState(null)
+  const [loggedInUserData, setLoggedInUserData] = useState(null)
   const authData = useContext(AuthContext)
+  
+  // useEffect(() => {
+  //   if(authData){
+  //     const loggedInUser = localStorage.getItem('loggedInUser');
+
+  //     if(loggedInUser){
+  //       setUser(loggedInUser.role);
+  //     }
+  //   }
+  // }, [authData])
+  
 
   const handleLogin =(email,password)=>{
     if(email == "admin@me.com" && password == '123'){
       setUser('admin')
+      localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'}));
     }
-    else if(authData && authData.employees.find((e)=> e.email == email && e.password == password)){
-      setUser("employee")
+    else if(authData){
+      const employee = authData.employees.find((e)=> e.email == email && e.password == password)
+      if(employee){
+        setUser("employee")
+        setLoggedInUserData(employee)
+        localStorage.setItem('loggedInUser',JSON.stringify({role:'employee'}));
+      }
     }
     else{
       alert("Invalid Credentials");
@@ -25,7 +43,7 @@ const App = () => {
   return (
     <>
       {!user ? <Login handleLogin={handleLogin}/>:''}
-      {user == 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
+      {user == 'admin' ? <AdminDashboard /> : (user == 'employee' ? <EmployeeDashboard data={loggedInUserData}/> : null)}
     </>
   )
 }
